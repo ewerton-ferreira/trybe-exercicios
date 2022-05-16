d19ae15f23e81908c0751d3eb4012f77da03c451
Ewerton Ferreira <ewertonferreira@msn.com>
2022-05-16 12:36:25 -0300

    Operações Simples

--- 01-fundamentos/b4-introdução-javascript-e-lógica-de-programação/d01-introducao-ao-javascript-primeiros-passos/p2/script.js
+++ 01-fundamentos/b4-introdução-javascript-e-lógica-de-programação/d01-introducao-ao-javascript-primeiros-passos/p2/script.js
@@ -1,17 +1,9 @@
-let patientId = 50;
-let isEnrolled = true;
-const patientInfo = {
-    firstName: "Ana",
-    lastName: "Santos",
-};
+const base = 5;
+let height = 8;
+const area = (base * height);
 
-const patientEmail = "ana@email.com"
-let patientAge = 31;
-patientId = "50";
+console.log(area);
 
+const perimeter = (base * 2 + height * 2)
 
-console.log(typeof patientId);
-console.log(typeof isEnrolled);
-console.log(typeof patientInfo);
-console.log(typeof patientEmail);
-console.log(typeof patientAge);
+console.log (perimeter);
