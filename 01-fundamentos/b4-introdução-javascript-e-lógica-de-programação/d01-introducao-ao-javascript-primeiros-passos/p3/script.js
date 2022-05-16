eb5a7faea4a96af9b9c34ae0522dccb427718833
Ewerton Ferreira <ewertonferreira@msn.com>
2022-05-16 13:04:21 -0300

    Condições If e Else

--- 01-fundamentos/b4-introdução-javascript-e-lógica-de-programação/d01-introducao-ao-javascript-primeiros-passos/p3/script.js
+++ 01-fundamentos/b4-introdução-javascript-e-lógica-de-programação/d01-introducao-ao-javascript-primeiros-passos/p3/script.js
@@ -1,26 +1,10 @@
-//if - se
-//else - senão
+const nota = 58;
 
-// if (condição) {
-//     //código
-// }
-// else if (outraCondição) {
-//     //outra condição
-// }
-// else {
-//     //condição
-// }
-
-let trybe = 18.41;
-
-if (trybe>= 14 && trybe <14.40) {
-    console.log("Esquenta")
-} else if (trybe >= 16.30 && trybe < 17.50) {
-    console.log("Aula ao vivo")
-} else if (trybe >=19.40 && trybe <20) {
-    console.log("Fechamento")
+if (nota >= 80) {
+    console.log ("Parabéns, você foi aprovada(o)!")
+} else if (nota <= 79 && nota >=60) {
+    console.log ("Você está na nossa lista de espera.")
 } else {
-    console.log("Fora dos momentos síncronos")
+    console.log ("Você foi reprovada(o).")
 }
-
 
