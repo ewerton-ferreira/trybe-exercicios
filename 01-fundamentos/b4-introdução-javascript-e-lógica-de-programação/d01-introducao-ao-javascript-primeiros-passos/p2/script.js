a47c6da321df031b7e3076c4988a73679cd69d1b
Ewerton Ferreira <ewertonferreira@msn.com>
2022-05-16 12:24:16 -0300

    Testando operações aritiméticas

--- 01-fundamentos/b4-introdução-javascript-e-lógica-de-programação/d01-introducao-ao-javascript-primeiros-passos/p2/script.js
+++ 01-fundamentos/b4-introdução-javascript-e-lógica-de-programação/d01-introducao-ao-javascript-primeiros-passos/p2/script.js
@@ -1,5 +1,14 @@
-let movie = "Batman"; //string lietral
-let score = 9.80; //number literal
-let isValid = true; // boolean
-let name; //undefined
-let color = null; //redefinição
+// let movie = "Batman"; //string lietral
+// let score = 9.80; //number literal
+// let isValid = true; // boolean
+// let name; //undefined
+// let color = null; //redefinição
+
+let salary = 3500;
+
+console.log(salary + salary);
+console.log(salary - salary);
+console.log(salary * salary);
+console.log(salary / salary);
+console.log(3 ** 4);
+
