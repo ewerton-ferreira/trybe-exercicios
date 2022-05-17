ac14785262da2c846d921501d940ee44dd32b92e
Ewerton Ferreira <ewertonferreira@msn.com>
2022-05-17 20:55:46 -0300

    Q05 resolvendo p2

--- 01-fundamentos/b4-introdução-javascript-e-lógica-de-programação/d02-array-e-loop-for/exercicio05.js
+++ 01-fundamentos/b4-introdução-javascript-e-lógica-de-programação/d02-array-e-loop-for/exercicio05.js
@@ -1,8 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let higherNumber = 0;
+
 for (let index = 0; index < numbers.length; index +=1) {
-    if(higherNumber > numbers[index])
-    resultado = higherNumber > numbers[index]
+    if(higherNumber > numbers[index]) {
+    higherNumber = numbers[index];
+}
 }
 
-console.log(resultado);
+console.log(higherNumber);
