62d413f3f07ac1ef18f6021c2ca8f086a67961e5
Ewerton Ferreira <ewertonferreira@msn.com>
2022-05-16 21:40:17 -0300

    para fixar p4

---
+++ 01-fundamentos/b4-introdução-javascript-e-lógica-de-programação/d01-introducao-ao-javascript-primeiros-passos/p4/script4.js
@@ -0,0 +1,19 @@
+let resultado = "verde";
+
+switch (resultado) {
+    case "azul": 
+        console.log("Aprovada(o)");
+        break;
+    
+    case "preto":
+        console.log("Listada(o)");
+        break;
+        
+    case "vermelho":
+        console.log("Reprovada(o)");
+        break;
+        
+    default:
+        console.log("Não se aplica")
+
+}
