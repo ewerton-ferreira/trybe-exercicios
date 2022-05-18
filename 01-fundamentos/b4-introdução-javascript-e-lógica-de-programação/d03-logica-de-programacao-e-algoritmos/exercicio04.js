84796c0b7907c51640b9b4107c1766a7d286cf58
Ewerton Ferreira <ewertonferreira@msn.com>
2022-05-18 13:36:18 -0300

    Q04 - Resolvendo

---
+++ 01-fundamentos/b4-introdução-javascript-e-lógica-de-programação/d03-logica-de-programacao-e-algoritmos/exercicio04.js
@@ -0,0 +1,12 @@
+let biggestPrimeNumber = 0;
+
+for (let index = 2; index <= 50; index +=1) {
+    let isPrime = true
+
+    for (let divider = 2; divider < index; divider +=1) {
+        if(index % divider === 0) {
+            isPrime = false
+        }
+    }
+}
+console.log(biggestPrimeNumber);
