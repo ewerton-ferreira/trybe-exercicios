0b6a51a4f42d6f9fc6d484e167eefde38b99dacd
Ewerton Ferreira <ewertonferreira@msn.com>
2022-05-19 15:34:32 -0300

    atualizando arquivo remoto

---
+++ 01-fundamentos/b4-introdução-javascript-e-lógica-de-programação/d03-logica-de-programacao-e-algoritmos/bonus01.js
@@ -0,0 +1,12 @@
+let main = '';
+let n = 5;
+
+for(let i = 1; i <= n; i +=1) {
+    main = main + '*';
+}
+
+console.log(main);
+console.log(main);
+console.log(main);
+console.log(main);
+console.log(main);
