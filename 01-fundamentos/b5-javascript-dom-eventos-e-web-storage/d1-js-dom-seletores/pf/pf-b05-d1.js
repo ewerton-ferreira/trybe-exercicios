1a1b26e0d1e14221d345074e06af63eff66c1beb
Ewerton Ferreira <ewertonferreira@msn.com>
2022-05-19 16:30:33 -0300

    PF1

---
+++ 01-fundamentos/b5-javascript-dom-eventos-e-web-storage/d1-js-dom-seletores/pf/pf-b05-d1.js
@@ -0,0 +1,22 @@
+const paragraph = document.getElementById("paragraph");
+      paragraph.style.color = "white";
+      paragraph.style.background = "blue";
+      paragraph.style.border = "1px solid black";
+      paragraph.style.width = "250px";
+      paragraph.style.textAlign = "center";
+      paragraph.style.padding = "5px";
+      paragraph.style.borderRadius = "10px";
+      paragraph.innerText = "Estudando muito";
+      document.getElementById("page-title").innerText = "A volta dos que não foram";
+      document.getElementById("subtitle").innerText = "Adianta o trabalho";
+
+document.getElementsByClassName("paragraph2")[0].style.color = "white";
+document.getElementsByClassName("paragraph2")[0].style.larger = "large";
+document.getElementsByTagName("h4")[0].style.background = "green";
+document.getElementsByTagName("h4")[0].style.width = "200px";
+document.getElementsByTagName("h4")[0].style.border = "2px solid blue";
+document.getElementsByTagName("h4")[0].style.borderRadius = "10px";
+document.getElementsByTagName("h4")[0].style.textAlign = "center";
+
+
+
