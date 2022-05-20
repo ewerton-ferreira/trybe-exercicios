f35327c709fd281df5410b976c8cf33741339596
Ewerton Ferreira <ewertonferreira@msn.com>
2022-05-20 11:38:33 -0300

    Q01

---
+++ 01-fundamentos/b5-javascript-dom-eventos-e-web-storage/d1-js-dom-seletores/script.js
@@ -0,0 +1,10 @@
+let tagsP = document.getElementsByTagName('p');
+tagsP[0].innerHTML = 'Espero estar trabalhando e avançando na carreira a cada dia. Buscando conhecimento para crescimento pessoal e profissional, para já ir tentando me tornar um dev pleno';
+tagsP[1].innerHTML = '';
+tagsP[2].innerHTML = '';
+
+
+
+
+
+
