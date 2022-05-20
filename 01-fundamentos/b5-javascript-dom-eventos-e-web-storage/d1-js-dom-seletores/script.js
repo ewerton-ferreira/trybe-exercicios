34312c7745b9a72169be56909851b827ba3b1651
Ewerton Ferreira <ewertonferreira@msn.com>
2022-05-20 14:15:22 -0300

    Funções inseridas

--- 01-fundamentos/b5-javascript-dom-eventos-e-web-storage/d1-js-dom-seletores/script.js
+++ 01-fundamentos/b5-javascript-dom-eventos-e-web-storage/d1-js-dom-seletores/script.js
@@ -1,27 +1,44 @@
 //Q01
-
-let tagsP = document.getElementsByTagName('p');
-tagsP[0].innerHTML = 'Espero estar trabalhando e avançando na carreira a cada dia. Buscando conhecimento para crescimento pessoal e profissional, para já ir tentando me tornar um dev pleno.';
-tagsP[1].innerHTML = '';
-tagsP[2].innerHTML = '';
+function changingTag() {
+    let tagsP = document.getElementsByTagName('p');
+        tagsP[0].innerHTML = 'Desejo estar trabalhando, avançando na carreira a cada dia, buscando vencer os desafios, acumular conhecimento para crescimento pessoal e profissional, no caminho para me tornar um desenvolvedor nível pleno, depois sênior.';
+        tagsP[1].innerHTML = '';
+        tagsP[2].innerHTML = '';
+}
+changingTag();
 
 //Q02
-document.getElementsByTagName('main')[0].style.backgroundColor = 'rgb(76,164,109)';
+function changeColor() {
+    let colorBg = document.getElementsByClassName('main-content');
+        colorBg[0].style.backgroundColor = 'rgb(76,164,109)';
+}
+changeColor();
 
 //Q03
-document.getElementsByClassName('center-content')[0].style.backgroundColor = 'white';
+function colorBgCenter() {
+    let changeBgCenter = document.getElementsByClassName('center-content');
+        changeBgCenter[0].style.backgroundColor = 'white';
+}
+colorBgCenter();
 
 //Q04
-document.getElementsByClassName('title')[0].innerHTML = 'Exercício 5.1 - JavaScript';
+function correctText() {
+    let text = document.getElementsByClassName('title');
+        text[0].innerHTML = 'Exercício 5.1 - JavaScript';
+}
+correctText();
 
 //Q05
-tagsP[0].style.textTransform = 'UpperCase';
+function upperCase() {
+    let upperC = document.getElementsByTagName('p');
+        upperC[0].style.textTransform = 'UpperCase';
+        upperC[0].style.textAlign = 'center'
+}
+upperCase();
 
 //Q06
-console.log(tagsP);
-
-
-
-
-
-
+function print() {
+    let displayTheContent = document.getElementsByTagName('p');
+    console.log(displayTheContent);
+}
+print();
