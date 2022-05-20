f5b353b2b09afda79532ee6c1ce932bd9bd4c916
Ewerton Ferreira <ewertonferreira@msn.com>
2022-05-20 13:28:10 -0300

    Finalizado

--- 01-fundamentos/b5-javascript-dom-eventos-e-web-storage/d1-js-dom-seletores/script.js
+++ 01-fundamentos/b5-javascript-dom-eventos-e-web-storage/d1-js-dom-seletores/script.js
@@ -1,7 +1,7 @@
 //Q01
 
 let tagsP = document.getElementsByTagName('p');
-tagsP[0].innerHTML = 'xxxxxxxxxxxxx xxxxxxxxxxxx xxxxxxxxxxxxx xxxxxxxxx xxxxxxxxx xxxxxxxx xxxxxxxxx xxxxxxxx xxxxxxxxxxxx xxxxxxxxxxx xxxxxxxx xxxxxxxxxxxxxx xxxxxxxxxxxxxx xxxxxxxxxxx.';
+tagsP[0].innerHTML = 'Espero estar trabalhando e avançando na carreira a cada dia. Buscando conhecimento para crescimento pessoal e profissional, para já ir tentando me tornar um dev pleno.';
 tagsP[1].innerHTML = '';
 tagsP[2].innerHTML = '';
 
@@ -15,7 +15,7 @@ document.getElementsByClassName('center-content')[0].style.backgroundColor = 'wh
 document.getElementsByClassName('title')[0].innerHTML = 'Exercício 5.1 - JavaScript';
 
 //Q05
-tagsP[0].innerHTML.toUpperCase = tagsP;
+tagsP[0].style.textTransform = 'UpperCase';
 
 //Q06
 console.log(tagsP);
