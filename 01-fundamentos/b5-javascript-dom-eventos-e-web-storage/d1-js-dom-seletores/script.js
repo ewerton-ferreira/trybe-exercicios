d85e235fc17727ef0e9e89483da3c08c772c3f20
Ewerton Ferreira <ewertonferreira@msn.com>
2022-05-20 12:47:48 -0300

    Q03

--- 01-fundamentos/b5-javascript-dom-eventos-e-web-storage/d1-js-dom-seletores/script.js
+++ 01-fundamentos/b5-javascript-dom-eventos-e-web-storage/d1-js-dom-seletores/script.js
@@ -6,9 +6,10 @@ tagsP[1].innerHTML = '';
 tagsP[2].innerHTML = '';
 
 //Q02
-let tagM = document.getElementsByTagName('main')[0].style.backgroundColor = 'rgb(76,164,109)';
+document.getElementsByTagName('main')[0].style.backgroundColor = 'rgb(76,164,109)';
 
 //Q03
+document.getElementsByClassName('center-content')[0].style.backgroundColor = 'white';
 
 
 
