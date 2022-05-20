9e3d879dc1662222aea8268ce735d49de8b38b62
Ewerton Ferreira <ewertonferreira@msn.com>
2022-05-20 10:24:57 -0300

    getElementsByTagName

--- 01-fundamentos/b5-javascript-dom-eventos-e-web-storage/d1-js-dom-seletores/pf/pf-b05-d1.js
+++ 01-fundamentos/b5-javascript-dom-eventos-e-web-storage/d1-js-dom-seletores/pf/pf-b05-d1.js
@@ -18,5 +18,13 @@ document.getElementsByTagName("h4")[0].style.border = "2px solid blue";
 document.getElementsByTagName("h4")[0].style.borderRadius = "10px";
 document.getElementsByTagName("h4")[0].style.textAlign = "center";
 
-
+let paragraph2 = document.querySelector("paragraph2");
+paragraph2.style.color = "white";
+paragraph2.style.larger = "large";
+// let h4 = document.querySelector('subtitle');
+// style.background = "green";      
+// subtitle.style.width = "200px";     
+// subtitle.style.border = "2px solid blue"; 
+// subtitle.style.borderRadius = "10px";     
+// subtitle.style.textAlign = "center";      
 
