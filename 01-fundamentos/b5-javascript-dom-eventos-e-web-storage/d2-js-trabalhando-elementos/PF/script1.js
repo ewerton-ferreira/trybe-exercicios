a7ed894722912244b4143d747736321fbd6c4b1a
Ewerton Ferreira <ewertonferreira@msn.com>
2022-05-22 20:32:28 -0300

    Entendendo elementos

--- 01-fundamentos/b5-javascript-dom-eventos-e-web-storage/d2-js-trabalhando-elementos/PF/script1.js
+++ 01-fundamentos/b5-javascript-dom-eventos-e-web-storage/d2-js-trabalhando-elementos/PF/script1.js
@@ -2,4 +2,4 @@
 
 console.log(document.getElementById('start').nextSibling) // nó
 
-// console.log(document.getElementById('start').nextElementSibling) // <p>elemento</p>
+console.log(document.getElementById('start').nextElementSibling) // <p>elemento</p>
