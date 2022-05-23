bd93d03765aed0fafce65f5c63452a05eea4b3db
Ewerton Ferreira <ewertonferreira@msn.com>
2022-05-22 21:08:31 -0300

    Exercícios PF1 - ok

---
+++ 01-fundamentos/b5-javascript-dom-eventos-e-web-storage/d2-js-trabalhando-elementos/PF/script2.js
@@ -0,0 +1,29 @@
+// Exercícios para fixar P1
+
+//Q01
+console.log(document.getElementById('elementoOndeVoceEsta').nextElementSibling.previousSibling.previousSibling)
+
+//Q02
+let test = document.getElementById('elementoOndeVoceEsta').parentElement;
+test.style.color = 'green';
+
+//Q03
+console.log(document.getElementById('elementoOndeVoceEsta').firstElementChild)
+
+//Q04
+console.log(document.getElementById('pai').firstElementChild)
+
+//Q05
+console.log(document.getElementById('elementoOndeVoceEsta').previousSibling.previousSibling)
+
+//Q06
+console.log(document.getElementById('elementoOndeVoceEsta').nextSibling)
+
+//Q07
+console.log(document.getElementById('elementoOndeVoceEsta').nextSibling.nextSibling)
+
+//Q08
+console.log(document.getElementById('pai').nextSibling.nextSibling);
+
+// Exercícios para fixar P2
+
