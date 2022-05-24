d6472b30a34f95553eec4143e5e973b382d31600
Ewerton Ferreira <ewertonferreira@msn.com>
2022-05-23 21:54:45 -0300

    Não consegui entender sem o gabarito

--- 01-fundamentos/b5-javascript-dom-eventos-e-web-storage/d3-js-eventos/PF/main.js
+++ 01-fundamentos/b5-javascript-dom-eventos-e-web-storage/d3-js-eventos/PF/main.js
@@ -11,18 +11,44 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+function insertClass (event) {
+    const classInsertTag = document.querySelector('.tech'); 
+    classInsertTag.classList.remove('tech');
+    event.target;classList.add('tech');
+    input;value = '';
+}
+
+firstLi.addEventListener('click', insertClass);
+secondLi.addEventListener('click', insertClass);
+thirdLi.addEventListener('click', insertClass);
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+
+input.addEventListener('input', function(event) {
+    const classInsertTag = document.querySelector('.tech'); 
+    classInsertTag.innerText = event.target.value;
+
+});
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
 
+myWebpage.addEventListener('dblclick', function() {
+    window.location.replace('https://github.com/ewerton-ferreira?tab=repositories');
+  });
+
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
 
 // Segue abaixo um exemplo do uso de event.target:
+    myWebpage.addEventListener('mouseover', function(event) {
+    event.target.style.color = 'red';
+  });
+    myWebpage.addEventListener('mouseout', function(event) {
+    event.target.style.color = 'unset';
+  });
 
 
 function resetText(event) {
