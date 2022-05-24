a1ac2c2bdcf720677a288bcded400177afa6f2c0
Ewerton Ferreira <ewertonferreira@msn.com>
2022-05-24 15:11:25 -0300

    Q02

--- 01-fundamentos/b5-javascript-dom-eventos-e-web-storage/d3-js-eventos/script.js
+++ 01-fundamentos/b5-javascript-dom-eventos-e-web-storage/d3-js-eventos/script.js
@@ -44,3 +44,16 @@ function createDaysOfTheWeek() {
     }
   }
   createDaysOfTheMonth();
+
+  //Q02
+
+  function createHolidayButton (buttonName) {
+      let buttonContainer = document.querySelector('.buttons-container');
+      let newButton= document.createElement('button');
+      let newButtonID = 'btn-holiday';
+
+        newButton.innerHTML = buttonName;
+        newButton.id = newButtonID;
+        buttonContainer.appendChild(newButton);
+  }
+  createHolidayButton('Feriados');
