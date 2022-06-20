6db627bca7f04fec1906597fe635eb23cf310bee
Ewerton Ferreira <ewertonferreira@msn.com>
2022-06-20 11:48:50 -0300

    Q03

--- 01-fundamentos/b7-introdução-a-javascript-es6-e-testes-unitarios/d1-JavaScript-ES6-let-const-arrow-functions-e-template-literals/script2.js
+++ 01-fundamentos/b7-introdução-a-javascript-es6-e-testes-unitarios/d1-JavaScript-ES6-let-const-arrow-functions-e-template-literals/script2.js
@@ -40,3 +40,8 @@ console.log(longestWord2('Antônio foi ao banheiro e não sabemos o que acontece
 
 //Q03
 
+let clickCount = 0;
+const textToDisplay = document.getElementById('text');
+const buttonTest = document.getElementById('button_test');
+
+buttonTest.addEventListener('click', () => textToDisplay.innerHTML = clickCount += 1);
