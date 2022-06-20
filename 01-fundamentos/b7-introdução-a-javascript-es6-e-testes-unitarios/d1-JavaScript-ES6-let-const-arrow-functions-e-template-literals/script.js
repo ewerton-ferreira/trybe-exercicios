4aad51cedb9d8fd81c518443f56c9c84d31fc0c7
Ewerton Ferreira <ewertonferreira@msn.com>
2022-06-20 11:15:54 -0300

    Q01 completa

--- 01-fundamentos/b7-introdução-a-javascript-es6-e-testes-unitarios/d1-JavaScript-ES6-let-const-arrow-functions-e-template-literals/script.js
+++ 01-fundamentos/b7-introdução-a-javascript-es6-e-testes-unitarios/d1-JavaScript-ES6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,3 +1,5 @@
+// Q01
+
 const testingScope = (escopo) => {
   if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -10,3 +12,34 @@ const testingScope = (escopo) => {
 }
 
 testingScope(true);
+
+// Q02
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens = () => {
+  oddsAndEvens[0] = 2;
+  oddsAndEvens[1] = 3;
+  oddsAndEvens[2] = 4;
+  oddsAndEvens[3] = 7; 
+  oddsAndEvens[4] = 10;
+  oddsAndEvens[5] = 13;
+
+  return oddsAndEvens;
+};
+
+const sortedArray = sortOddsAndEvens();
+console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente!`);
+
+// Bônus
+
+const oddsAndEvens2 = [13, 3, 4, 10, 7, 2, 1, 8, 15, 19, 21];
+
+const sortArrayBonus = (array) => {
+  const sortOddsAndEvens = array.sort((a, b) => a - b);
+  return sortOddsAndEvens;
+}
+
+const sortedArrayBonus = sortArrayBonus(oddsAndEvens2);
+console.log(`Os números ${sortedArrayBonus} se encontram ordenados de forma crescente`)
+console.log(`Os números ${oddsAndEvens2.sort((a, b) => a - b)} se encontram ordenados de forma crescente`);
