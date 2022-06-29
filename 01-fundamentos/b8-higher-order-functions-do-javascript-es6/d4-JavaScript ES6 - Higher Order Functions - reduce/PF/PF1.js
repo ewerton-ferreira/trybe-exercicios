05c43b795ad040553e1f3d930603703b227a06d5
Ewerton Ferreira <ewertonferreira@msn.com>
2022-06-28 21:08:19 -0300

    PF1 8.4

---
+++ 01-fundamentos/b8-higher-order-functions-do-javascript-es6/d4-JavaScript ES6 - Higher Order Functions - reduce/PF/PF1.js
@@ -0,0 +1,210 @@
+// //Com for
+// const numbers = [32, 15, 3, 2, -5, 56, 10];
+// let sumNumbers = 0;
+// // A variável 'acumula', a cada iteração do for, o resultado da operação feita lá!
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   sumNumbers += numbers[index];
+// }
+// console.log(sumNumbers); // 113
+
+
+
+
+//Com reduce
+
+// const numbers = [32, 15, 3, 2, -5, 56, 10];
+
+// const sumNumbers = numbers.reduce((result, number) => result + number); // O parâmetro `result` é o acumulador. Ele recebe, do `reduce`, o retorno da função a cada iteração.
+// console.log(sumNumbers); // 113
+
+// // Ou seja:
+
+// const numbers2 = [12, 35, 23, 20, -15, 16, 30];
+
+// const getSum = (result, number) => result + number;
+// const sumNumbers2 = numbers2.reduce(getSum);
+// console.log(sumNumbers2); // 121
+
+
+
+// const collection = [1, 2, 3, 4, 5];
+
+// const getSum = (accumulator, number) => {
+//   console.log(accumulator); // 1 3 6 10
+//   return accumulator + number;
+// };
+
+// const sumNumbers = collection.reduce(getSum);
+// console.log(sumNumbers); // 15
+
+
+
+
+// const numbers = [32, 15, 3, 2, -5, 56, 10];
+
+// const getSum = (result, number) => {
+//   console.log(result); // 32 47 50 52 47 103
+//   return result + number;
+// };
+
+// const sumNumbers = numbers.reduce(getSum);
+// console.log(sumNumbers); // 113
+
+
+
+
+// const numbers = [32, 15, 3, 2, -5, 56, 10];
+
+// const getSum = (result, number) => {
+//   console.log(result); // 0 32 47 50 52 47 103
+//   return result + number;
+//   };
+// const sumNumbers = numbers.reduce(getSum, 0); // Parâmetro adicionado ao reduce: o "0"
+// console.log(sumNumbers); // 113
+
+
+
+
+// const numbers = [32, 15, 3, 2, -5, 56, 10];
+
+// const getSum = (result, number) => {
+//   console.log(result); // 10 42 57 60 62 57 113
+//   return result + number;
+//   };
+// const sumNumbers = numbers.reduce(getSum, 10);
+// console.log(sumNumbers); // 123
+
+
+
+
+// const numbers = [50, 85, -30, 3, 15];
+
+// const getBigger = (bigger, number) => ((bigger > number) ? bigger : number);
+
+// const bigger = numbers.reduce(getBigger, 0);
+// console.log(bigger); // 85
+
+
+
+// const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+// const getEven = (number) => number % 2 === 0;
+// const sumPair = (accumulator, number) => accumulator + number;
+
+// const sumNumbers = (array) => array.filter(getEven).reduce(sumPair); // Olhe que código pequeno e conciso!
+
+// console.log(sumNumbers(numbers)); // 152
+
+
+
+
+// const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+// const sumPair = (accumulator, number) => (
+//   (number % 2 === 0) ? accumulator + number : accumulator
+// );
+
+// const sumNumbers = (array) => array.reduce(sumPair, 0);
+
+// console.log(sumNumbers(numbers)); // 152
+
+
+
+
+const estudantes = [
+  {
+    nome: 'Jorge',
+    sobrenome: 'Silva',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 67 },
+      { name: 'Português', nota: 79 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 65 },
+    ],
+  },
+  {
+    nome: 'Mario',
+    sobrenome: 'Ferreira',
+    idade: 15,
+    turno: 'Tarde',
+    materias: [
+      { name: 'Matemática', nota: 59 },
+      { name: 'Português', nota: 80 },
+      { name: 'Química', nota: 78 },
+      { name: 'Biologia', nota: 92 },
+    ],
+  },
+  {
+    nome: 'Jorge',
+    sobrenome: 'Santos',
+    idade: 15,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 76 },
+      { name: 'Português', nota: 90 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 80 },
+    ],
+  },
+  {
+    nome: 'Maria',
+    sobrenome: 'Silveira',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 91 },
+      { name: 'Português', nota: 85 },
+      { name: 'Química', nota: 92 },
+      { name: 'Biologia', nota: 90 },
+    ],
+  },
+  {
+    nome: 'Natalia',
+    sobrenome: 'Castro',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 70 },
+      { name: 'Português', nota: 70 },
+      { name: 'Química', nota: 60 },
+      { name: 'Biologia', nota: 50 },
+    ],
+  },
+  {
+    nome: 'Wilson',
+    sobrenome: 'Martins',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 80 },
+      { name: 'Português', nota: 82 },
+      { name: 'Química', nota: 79 },
+      { name: 'Biologia', nota: 75 },
+    ],
+  },
+];
+
+//Resultado esperado
+// [
+//   { name: 'Jorge', materia: 'Português' },
+//   { name: 'Mario', materia: 'Biologia' },
+//   { name: 'Jorge', materia: 'Português' },
+//   { name: 'Maria', materia: 'Química' },
+//   { name: 'Natalia', materia: 'Português' },
+//   { name: 'Wilson', materia: 'Português' },
+// ]
+
+
+// const getBestClass = (acc, materia) => {
+//   if (acc.nota > materia.nota) return acc;
+//   return materia;
+// };
+
+// const reportBetter = (students) => students.map((student) => ({
+//   name: student.nome,
+//   materia: student.materias.reduce(getBestClass).name}));
+
+// console.log(reportBetter(estudantes));
