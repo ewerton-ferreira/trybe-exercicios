580b09f1c043d268ee4d353f307b66e6edafdc0e
Ewerton Ferreira <ewertonferreira@msn.com>
2022-07-18 19:34:42 -0300

    Exercício da aula 9.2

--- 02-front-end/b9 - JavaScript e Testes Assíncronos/d2-JavaScript-Assincrono-Fetch-API-e-async-await/api.js
+++ 02-front-end/b9 - JavaScript e Testes Assíncronos/d2-JavaScript-Assincrono-Fetch-API-e-async-await/api.js
@@ -1,14 +1,13 @@
-const fetch = require('node-fetch');
-
+// api.js
 const fetchCoins = async () => {
   const url = 'https://api.coincap.io/v2/assets';
 
   const coins = await fetch(url)
     .then((response) => response.json())
     .then((data) => data.data)
-    .catch((error) => error.toString())
+    .catch((error) => error.toString());
 
-    return coins;
+  return coins;
 }
 
 const setCoins = async () => {
@@ -16,14 +15,15 @@ const setCoins = async () => {
 
   const coinsList = document.getElementById('coins-list');
 
-  coins.filter((coin) => Number(coin.rank) <= 10)
-  .forEach((coin) => {
-    const newLi = document.createElement('li');
+  coins
+    .filter((_, index) => index < 10)
+    .forEach((coin) => {
+      const newLi = document.createElement('li');
 
-    newLi.innerText = `${coin.name} (${coin.symbol}): ${usdPrice.toFixed(2)}`;
+      newLi.innerText = `${coin.name} (${coin.symbol}): ${coin.priceUsd}`;
 
-    coinsList.appendChild(newLi);
-  });
+      coinsList.appendChild(newLi);
+    });
 }
 
 window.onload = () => setCoins();
