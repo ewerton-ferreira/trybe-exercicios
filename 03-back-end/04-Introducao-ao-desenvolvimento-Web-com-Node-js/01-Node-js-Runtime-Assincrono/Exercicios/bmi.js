11df187cd1104e74e29b15a8b56e7bdd71f1339b
Ewerton Ferreira <ewertonferreira@msn.com>
2022-11-01 16:15:49 -0300

    Exercício 03

--- 03-back-end/04-Introducao-ao-desenvolvimento-Web-com-Node-js/01-Node-js-Runtime-Assincrono/Exercicios/bmi.js
+++ 03-back-end/04-Introducao-ao-desenvolvimento-Web-com-Node-js/01-Node-js-Runtime-Assincrono/Exercicios/bmi.js
@@ -1,8 +1,28 @@
-const weightInKg = 95; // peso em kilos
-const heightInCm = 174; // altura em centímetros
+const readline = require('readline-sync');
+
+// const weightInKg = 95; // peso em kilos
+// const heightInCm = 174; // altura em centímetros
+
+// function handleBMI(weight, height) {
+//   console.log(`Weight: ${weight}, Height: ${height}`);
+
+//   const heightInMeters = height / 100;
+//   const heightSquared = heightInMeters ** 2;
+//   const bmi = weight / heightSquared;
+
+//   return bmi;
+// }
+
+// function main() {
+//   const bmi = handleBMI(weightInKg, heightInCm);
+//   console.log(`BMI: ${bmi.toFixed(2)}`);
+// }
+
+// main();
+
+//REQUISITO 03
 
 function handleBMI(weight, height) {
-  console.log(`Weight: ${weight}, Height: ${height}`);
 
   const heightInMeters = height / 100;
   const heightSquared = heightInMeters ** 2;
@@ -12,7 +32,10 @@ function handleBMI(weight, height) {
 }
 
 function main() {
-  const bmi = handleBMI(weightInKg, heightInCm);
+  const weight = readline.question("What\'s your weight? "); // peso em kilos
+  const height = readline.question("What\'s your height? "); // altura em centímetros
+  
+  const bmi = handleBMI(weight, height);
   console.log(`BMI: ${bmi.toFixed(2)}`);
 }
 
