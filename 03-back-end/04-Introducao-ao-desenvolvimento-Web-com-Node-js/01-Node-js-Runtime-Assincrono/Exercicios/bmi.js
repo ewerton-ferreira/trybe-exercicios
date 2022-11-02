b10cfe0f5f97621b6d189c0da0bb1185279927cf
Ewerton Ferreira <ewertonferreira@msn.com>
2022-11-02 02:57:36 -0300

    Requisito 6-a

--- 03-back-end/04-Introducao-ao-desenvolvimento-Web-com-Node-js/01-Node-js-Runtime-Assincrono/Exercicios/bmi.js
+++ 03-back-end/04-Introducao-ao-desenvolvimento-Web-com-Node-js/01-Node-js-Runtime-Assincrono/Exercicios/bmi.js
@@ -167,3 +167,20 @@ const path = require('path');
 
 // ******************************************************************
 
+//REQUISITO 06-A
+
+
+async function readAll() {
+  const fileContent = await fs.readFile('./simpsons.json', 'utf-8');
+  const simpsons = JSON.parse(fileContent);
+  const strings = simpsons.map(({ id, name }) => `${id} - ${name}`);
+
+  strings.forEach((string) => console.log(string));
+}
+
+
+async function main() {
+  await readAll();
+}
+
+main();
